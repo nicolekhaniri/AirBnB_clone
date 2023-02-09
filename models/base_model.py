207d42c388d8360ad225f4ea4c0386db68eb51c0
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-02-09 12:24:44 +0300

    console removed and 3 written out

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+"""Class BaseModel that defines all common attributes/methods for other classes"""
+class BaseModel:
+    """Public instance attribute"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    """Public instance methods"""
+    def save(self):
+        """updating time to now"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary containing keys and values of instances"""
+        dict_rep = self.__dict__.copy()
+        dict_rep["class"] = self.__class__.__name__
+        dict_rep["created_at"] = self.created_at.isformat()
+        dict_rep["updated_at"] = self.updated_at.isformat()
+        return dict_rep
+    
+
+    """This is last"""
+    def __str__(self):
+        """To return the str rep of BaseModel instance"""
+        clsn = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsn, self.id, __dict__)
