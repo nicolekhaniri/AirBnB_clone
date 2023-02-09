e2e8313807191fd0d96c369cb8a1b72381461d9d
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-02-09 15:53:32 +0300

    user

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,19 +16,22 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as s:
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             saves = {}
             for key, value in FileStorage.__objects.items():
                 saves[key] = value.to_dict()
-            json.dump(saves ,s)
+            json.dump(saves ,f)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as r:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 objects_dict = json.load(f)
                 for key, value in objects_dict.items():
-                    class_name = value['__class__']
-                    del value['__class__']
-                    FileStorage.__objects[key] = globals()[class_name](**value)
+                    class_name = value["__class__"]
+                    if class_name == "BaseModel":
+                        FileStorage.__objects[key] = BaseModel(**obj_dict)
+                    if class_name == "User":
+                         FileStorage.__objects[key] = User(**obj_dict)
+
         except FileNotFoundError:
             pass
--- models/user.py
+++ models/user.py
@@ -2,4 +2,8 @@
 from models.base_model import BaseModel
 
 class User(BaseModel):
-
+    """Public class atributes"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
